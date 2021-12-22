16a18fa99b76872256b17458cb95b4eb6e4ee4fa
Bkroland19 <rkbukenya@cis.mak.ac.ug>
2021-12-22 13:07:50 +0300

    joi setup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const Joi = require("joi");
 //lets call the created express object
 
 const app = express();
@@ -21,6 +21,31 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
+	//joi validations
+	// const schema = {
+	// 	name: Joi.string().min(3).required(),
+	// };
+
+	// const result = Joi.validate(req.body, schema);
+	// console.log(result);
+
+	//newer version of join
+
+	const schema = Joi.object({
+		name: Joi.string().min(3).required(),
+	});
+
+	const validation = schema.validate(req.body);
+	res.send(validation);
+
+	console.log(validation);
+
+	//validations
+	if (validation.error) {
+		//400 bad request
+		res.status(400).send(validation.error.details[0].message);
+		return;
+	}
 	const course = {
 		id: courses.length + 1,
 		name: req.body.name,
@@ -34,8 +59,27 @@ app.post("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
 	const course = courses.find((c) => c.id === parseInt(req.params.id));
-
 	if (!course) res.status(404).send("No such course");
+	res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
+	if (!course) res.status(404).send("No such course");
+
+	const schema = Joi.object({
+		name: Joi.string().min(3).required(),
+	});
+
+	const validation = schema.validate(req.body);
+	if (validation.error) {
+		//400 bad request
+		res.status(400).send(validation.error.details[0].message);
+		return;
+	}
+
+	course.name = req.body.name;
+
 	res.send(course);
 });
 
