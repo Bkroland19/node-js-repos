e00f7f595062edbeb87c299ca273a7e0dee1fb51
Bkroland19 <rkbukenya@cis.mak.ac.ug>
2021-12-22 12:00:18 +0300

    adding http post request

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require("express");
 //lets call the created express object
 
 const app = express();
-
+//here we are adding a piece of middleware
+app.use(express.json());
 
 const courses = [
-    {id:1 ,name = 'course1'},
-    {id:2 ,name = 'course2'},
-    {id:3 ,name = 'course3'}
-
-]
+	{ id: 1, name: "course1" },
+	{ id: 2, name: "course2" },
+	{ id: 3, name: "course3" },
+];
 
 app.get("/", (req, res) => {
 	res.send("Hello Node");
@@ -20,11 +20,23 @@ app.get("/api/courses", (req, res) => {
 	res.send(courses);
 });
 
+app.post("/api/courses", (req, res) => {
+	const course = {
+		id: courses.length + 1,
+		name: req.body.name,
+	};
+
+	courses.push(course);
+	res.send(course);
+});
+
 //retrieving a single courses
 
 app.get("/api/courses/:id", (req, res) => {
-	const course = courses.find(c=>c.id === parseInt(req.params.id) );
-    res.send(course);
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
+
+	if (!course) res.status(404).send("No such course");
+	res.send(course);
 });
 
 const port = process.env.PORT || 3000;
